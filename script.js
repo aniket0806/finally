b10138b63d337ce842b6ce0335fd0802e04a72f1
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2022-01-30 23:50:07 +0530

    changing done

--- script.js
+++ script.js
@@ -10,16 +10,3 @@ document.querySelector("#close i").addEventListener("click" ,function(){
   
 });
 
-// document.querySelector("#close i").addEventListener("click" ,function(){
-   
-//   document.querySelector("#about").style.display="none"
-//   // document.querySelector("#main1").style.display="initial"
-  
-// });
-var tl
-$('.container3').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
